6107677948fd65fc9018e432f90b939e0b1104b1
Gabriel Trabanco Llano <gtrabanco@users.noreply.github.com>
2021-03-04 22:29:29 +0100

    Fixed timeout to avoid twitch limitations

--- src/index.ts
+++ src/index.ts
@@ -68,7 +68,7 @@ function onMessageHandler (target:any, context:any, msg:any, self:any) {
 
   // If the command is known, let's execute it
   if (commandName === '!troll') {
-    setTimeout(()=> client.say(target, `!troll`), 1000);
+    setTimeout(()=> client.say(target, `!troll`), 30000);
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
